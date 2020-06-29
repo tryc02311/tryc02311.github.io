00b4be975812185b7bae1e1aade48718d2d7b079
Patricia Rutledge <tryc02311@users.noreply.github.com>
2020-06-28 19:59:59 -0500

    edit minor details

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,25 @@
 //declarations
 const modal = document.querySelector('.modal');
 const modalWindow = document.querySelector('.modal__window');
-const edit = document.querySelector('.profile__edit-btn');
+const edit = document.querySelector('.profile__edit-button');
 const name = document.querySelector('.profile__name');
 const editName = document.querySelector('.edit_name');
 const job = document.querySelector('.profile__job');
 const editJob = document.querySelector('.edit_job');
-const save = document.querySelector('.modal__save-btn');
-const close = document.querySelector('.modal__close-btn');
-const add = document.quer
+const save = document.querySelector('.modal__save-button');
+const close = document.querySelector('.modal__close-button');
+//In the learning platform we were taught to use btn instead of button, so I am a little confused.
 
 
 //changes modal display from 'none' to 'flex'; open modal 
-// closes when `.modal__close-btn` or `.modal__save-btn` are clicked
+// closes when `.modal__close-btn` or `.modal__save-button` are clicked
 function modalDisplay() {
   modal.classList.toggle('modal');
 }
 
 //edit and apply changes to profile
-//prevent default load upon clicking '.modal__save-btn'
-//call modalClose when '.modal__save-btn' is clicked
+//prevent default load upon clicking '.modal__save-button'
+//call modalClose when '.modal__save-button' is clicked
 function update(event) {
   name.textContent = editName.value;
   job.textContent = editJob.value;
@@ -29,11 +29,11 @@ function update(event) {
   modalDisplay()
 }
 
-//open modal when '.profile__edit-btn' is clicked
+//open modal when '.profile__edit-button' is clicked
 edit.addEventListener('click', modalDisplay);
 
-//save and close when '.modal__save-btn' is clicked
+//save and close when '.modal__save-button' is clicked
 save.addEventListener('click',update);
 
-//close modal when '.modal__close-btn' or 'modal__save-btn' are clicked
+//close modal when '.modal__close-button' or 'modal__save-button' are clicked
 close.addEventListener('click', modalDisplay);
